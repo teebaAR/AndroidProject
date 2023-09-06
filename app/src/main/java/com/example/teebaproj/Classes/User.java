57cb06eb389eaac5d2466ccc81dfdedf2ceaa57b
teebaAR <teebaaburashid55@gmail.com>
2023-09-06 14:24:48 +0300

    user

---
+++ app/src/main/java/com/example/teebaproj/Classes/User.java
@@ -0,0 +1,10 @@
+package com.example.teebaproj.Classes;
+
+public class User {
+    private String email;
+    private String password;
+    public User(String email, String password){
+        this.email=email;
+        this.password=password;
+    }
+}
