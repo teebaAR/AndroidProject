ace5859a19e78f61ca1a51bdec1be9d0a1c9ddfd
teebaAR <teebaaburashid55@gmail.com>
2023-09-13 14:26:49 +0300

    user

--- app/src/main/java/com/example/teebaproj/LogInActivity.java
+++ app/src/main/java/com/example/teebaproj/LogInActivity.java
@@ -1,23 +1,41 @@
 package com.example.teebaproj;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogInActivity extends AppCompatActivity {
+
+    EditText emailEditText;
+    EditText passwordEditText;
+    Button  loginButton;
+    TextView  newAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        emailEditText=findViewById(R.id.etemail);
+         emailEditText = findViewById(R.id.etemail);
         passwordEditText=findViewById(R.id.etpassword);
         loginButton=findViewById(R.id.btLogIn);
         newAccountButton=findViewById(R.id.goToSignUp);
         newAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this,CreateNewAcountActivity.class));
+                startActivity(new Intent(LogInActivity.this,SignUpActivity.class));
             }
         });
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -25,14 +43,14 @@ public class LogInActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 final FirebaseAuth mAuth=FirebaseAuth.getInstance();
-                mAuth.signInWithEmailAndPassword(etemail.getText().toString(), etpassword.getText().toString())
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                        .addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     FirebaseUser user = mAuth.getCurrentUser();
 
-                                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                                    startActivity(new Intent(LogInActivity.this,MainActivity.class));
 
                                 } else {
 
--- app/src/main/java/com/example/teebaproj/SignUpActivity.java
+++ app/src/main/java/com/example/teebaproj/SignUpActivity.java
@@ -1,14 +1,107 @@
 package com.example.teebaproj;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText fullNameEditText;
+    EditText emailEditText;
+    EditText passwordEditText;
+    EditText repasswordEditText;
+    Button registerButton;
+   TextView errorText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        fullNameEditText = findViewById(R.id.name);
+        emailEditText=findViewById(R.id.etemail2);
+        passwordEditText=findViewById(R.id.etpassword2);
+        repasswordEditText=findViewById(R.id.etconfirmPassword);
+        registerButton=findViewById(R.id.btSignUp);
+        errorText=findViewById(R.id.errorText);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewAccount();
+            }
+        });
+    }
+    private void createNewAccount(){
+        errorText.setVisibility(View.GONE);
+        if(fullNameEditText.getText().toString().equals("")){
+            errorText.setVisibility(View.VISIBLE);
+            errorText.setText("please enter your full name");
+            return;
+        }
+        if(emailEditText.getText().toString().equals("")){
+            errorText.setVisibility(View.VISIBLE);
+            errorText.setText("please enter your email");
+            return;
+        }
+        if(passwordEditText.getText().toString().equals("")){
+            errorText.setVisibility(View.VISIBLE);
+            errorText.setText("please enter your password");
+            return;
+        }
+        if(repasswordEditText.getText().toString().equals("")){
+            errorText.setVisibility(View.VISIBLE);
+            errorText.setText("please confirm your password");
+            return;
+        }
+        if(!repasswordEditText.getText().toString().equals(passwordEditText.getText().toString())){
+            errorText.setVisibility(View.VISIBLE);
+            errorText.setText("password dosn't match");
+            return;
+        }
+        final FirebaseAuth mAuth=FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(fullNameEditText.getText().toString())
+                                    .build();
+
+                            user.updateProfile(profileUpdates)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                startActivity(new Intent(SignUpActivity.this,MainActivity.class));
+                                            }
+                                        }
+                                    });
+                        } else {
+
+                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            errorText.setVisibility(View.VISIBLE);
+                            errorText.setText(task.getException().getMessage());
+
+                        }
+                    }
+                });
     }
 }
